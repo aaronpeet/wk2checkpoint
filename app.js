df358f6e7a18fd419f5e812b4f28b47d987c467f
aaronpeet <aaronpeet@icloud.com>
2021-07-02 16:49:51 -0600

    click count/buyshovel update

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 
 let clickUpgrades = {
-    shovel: {
-        price: 50,
+    shovels: {
+        price: 5,
         quantity: 0,
         multiplier: 2
     },
     arms: {
-        price: 100,
+        price: 200,
         quantity: 0,
-        multiplier: 20
+        multiplier: 2
     }
 }
 
 let automaticUpgrades = {
-    monster: {
+    monsters: {
         price: 1000,
         quantity: 0,
-        multiplier: 100
+        multiplier: 2
     },
-    chuck: {
+    chucks: {
         price: 2500,
         quantity: 0,
-        multiplier: 1000
+        multiplier: 2
     }
 }
 
-let cheese = 0;
+let cheese = 0
+let shovel = 0
+let arm = 0
+let monster = 0
+let chuck = 0
 
 function mine() {
-    cheese = cheese +1
-    console.log(cheese)
-    document.getElementById('count').innerText= cheese
+    cheese = cheese + 1
+    document.getElementById('count').innerText = cheese
+    update()
+    return cheese
+}
+
+function buyShovel() {
+    if (cheese >= clickUpgrades.shovels.price) {
+        clickUpgrades.shovels.quantity += 1
+        cheese -= clickUpgrades.shovels.price
+        update()
+     }
+    document.getElementById('shovelCount').innerText = clickUpgrades.shovels.quantity
+}
+
+function purchMonster() {
+    if (cheese >= automaticUpgrades.monster.price) {
+        cheese = cheese - automaticUpgrades.monster.price
+    }
+}
+
+function update() {
+    cheese = cheese
+    document.getElementById('count').innerText = cheese
 }
 
