d441a6b028fa8ea174e7f968188eb14e7675ed2e
aaronpeet <aaronpeet@icloud.com>
2021-07-10 15:43:35 -0600

    buy functions

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ let clickUpgrades = {
         multiplier: 2
     },
     arms: {
-        price: 200,
+        price: 10,
         quantity: 0,
-        multiplier: 2
+        multiplier: 4
     }
 }
 
 let automaticUpgrades = {
     monsters: {
-        price: 1000,
+        price: 15,
         quantity: 0,
-        multiplier: 2
+        multiplier: 20
     },
     chucks: {
-        price: 2500,
+        price: 20,
         quantity: 0,
-        multiplier: 2
+        multiplier: 40
     }
 }
 
@@ -32,11 +32,13 @@ let cheese = 0
 //let chuck = 0
 
 function mine() {
-    cheese = cheese + 1
+         cheese += 1
+    
     document.getElementById('count').innerText = cheese
     update()
     return cheese
 }
+
 
 function buyShovel() {
     if (cheese >= clickUpgrades.shovels.price) {
@@ -48,17 +50,36 @@ function buyShovel() {
     return shovels.quantity
 }
 
-function clickUpgade() {
-    if (shovel = 1) {
-        mine() * 2
-    }
+function buyArms() {
+    if (cheese >= clickUpgrades.arms.price) {
+        clickUpgrades.arms.quantity += 1
+        cheese -= clickUpgrades.arms.price
+        update()
+     }
+    document.getElementById('armCount').innerText = clickUpgrades.arms.quantity
+    return arms.quantity
 }
 
 function buyMonster() {
     if (cheese >= automaticUpgrades.monsters.price) {
-        cheese = cheese - automaticUpgrades.monsters.price
-    }
+        automaticUpgrades.monsters.quantity += 1
+        cheese -= automaticUpgrades.monsters.price
+        update()
+     }
+    document.getElementById('monsterCount').innerText = automaticUpgrades.monsters.quantity
+    return monsters.quantity
 }
+
+function buyChuck() {
+    if (cheese >= automaticUpgrades.chucks.price) {
+        automaticUpgrades.chucks.quantity += 1
+        cheese -= automaticUpgrades.chucks.price
+        update()
+     }
+    document.getElementById('chuckCount').innerText = automaticUpgrades.chucks.quantity
+    return chucks.quantity
+}
+
 
 function update() {
     cheese = cheese
